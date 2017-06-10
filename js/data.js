7019f0eacc291993c7d0cdebbf6a1e2051fdf4bf
金成東 <songdong0204@gmail.com>
2017-06-10 21:07:39 +0900

    data.js追加。

---
+++ js/data.js
@@ -0,0 +1,95 @@
+{
+    "menu1": {
+        "win": {
+            "gu": "gyu",
+            "choki": "tai",
+            "par": "kani"
+        }
+    } {
+        "draw": {
+            "gu": "tori",
+            "choki": "shake",
+            "par": "ebi"
+        }
+    } {
+        "lose": {
+            "gu": "tofu",
+            "choki": "sirasu",
+            "par": "sijimi"
+        }
+    }
+}
+
+{
+    "menu2": {
+        "win": {
+            "gu": "moz-ruk",
+            "choki": "zuk-pap",
+            "par": "goy-tom"
+        }
+    } {
+        "draw": {
+            "gu": "hak-nin",
+            "choki": "jag-tam",
+            "par": "hou-dai"
+        }
+    } {
+        "lose": {
+            "gu": "eno-sir",
+            "choki": "kai-kon",
+            "par": "moy-fu"
+        }
+    }
+}
+
+{
+    "menu3": {
+        "win": {
+            "gu": "ibusu",
+            "choki": "ageru",
+            "par": "aburu"
+        }
+    } {
+        "draw": {
+            "gu": "itameru",
+            "choki": "niru",
+            "par": "musu"
+        }
+    } {
+        "lose": {
+            "gu": "yaku",
+            "choki": "taku",
+            "par": "renji"
+        }
+    }
+}
+
+{
+    "menu4": {
+        "win": {
+            "gu": "was-sho",
+            "choki": "but-kch",
+            "par": "wei-kat"
+        }
+    } {
+        "draw": {
+            "gu": "mis-may",
+            "choki": "hrb-nin",
+            "par": "oys-egg"
+        }
+    } {
+        "lose": {
+            "gu": "osu-wat",
+            "choki": "col-mas",
+            "par": "tou-san"
+        }
+    }
+}
+
+{
+    "menu5": {
+        "win": "xo"
+        "draw": "kochu"
+        "lose": "touban"
+    }
+}
