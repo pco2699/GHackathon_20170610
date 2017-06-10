293de263fa4bfa9a840083b54128dbcafb0f63ec
Taro Ohsugi <e501sibas32@gmail.com>
2017-06-10 19:44:23 +0900

    no message

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+    //ウインドウサイズをいい感じに合わせる
+    $(window).on('load resize', function () {
+        var h = $(window).height();
+        var w = $(window).width();
+    });
+
+
+
+
+
+
+});
