7f4709e486eb0227a7074812b251921fa3744208
Taro Ohsugi <e501sibas32@gmail.com>
2017-06-10 20:03:15 +0900

    jsテスト

--- js/script.js
+++ js/script.js
@@ -6,6 +6,57 @@ $(function () {
     });
 
 
+    $('.start_bt').on('click', function () {
+        $('.janken').fadeIn(1000);
+    });
+
+
+
+    $(".gu_btn").on("click", function () {
+        var humanGu = 1;
+        var com = Math.floor(Math.random() * 3 + 1);
+
+        if (com == 1) {
+            $("#pc").text("グー")
+            $("#result").text("あいこです")
+        } else if (com == 2) {
+            $("#pc").text("チョキ")
+            $("#result").text("勝ちです")
+        } else if (com == 3) {
+            $("#pc").text("パー")
+            $("#result").text("負けです")
+        }
+    });
+
+    $("#cho_btn").on("click", function () {
+        var humaCho = 2;
+        var com = Math.floor(Math.random() * 3 + 1);
+
+        if (com == 1) {
+            $("#pc").text("グー")
+            $("#result").text("負けです")
+        } else if (com == 2) {
+            $("#pc").text("チョキ")
+            $("#result").text("あいこです")
+        } else if (com == 3) {
+            $("#pc").text("パー")
+            $("#result").text("あいこです")
+        }
+    });
+    $("#par_btn").on("click", function () {
+        var humaPar = 3;
+        var comp = Math.floor(Math.random() * 3 + 1);
+        if (comp == 1) {
+            $("#pc").text("グー")
+            $("#result").text("勝ちです")
+        } else if (comp == 2) {
+            $("#pc").text("チョキ")
+            $("#result").text("負けdす")
+        } else if (comp == 3) {
+            $("#pc").text("パー")
+            $("#result").text("あいこです")
+        }
+    });
 
 
 
